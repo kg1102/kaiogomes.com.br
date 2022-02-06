e836cbf0557fa66f0ca125e1e82db4fb9fcf87a5
Kaio Gomes <KaioGomes@bugcrowdninja.com>
2022-02-06 20:49:35 -0300

    Created the simple content of /blog

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,16 @@
+import {Header} from '../../components/header/index';
+import styled from 'styled-components';
+
+export function BlogPage(){
+    const Container = styled.div`
+    
+    `;
+    return (
+        <>
+            <Header/>
+            <Container>
+                <h1>test</h1>
+            </Container>
+        </>
+    )
+}
