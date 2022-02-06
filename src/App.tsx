e6ca54d0686260fde4b0d7b15948aebf070379b6
Kaio Gomes <KaioGomes@bugcrowdninja.com>
2022-02-06 20:48:45 -0300

    Added Route /blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import { Homepage } from './pages/home/index';
+import { HomePage } from './pages/home/index';
+import { BlogPage } from './pages/blog/index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/blog/:id" element={<BlogPage/>}/>
       </Routes>
     </BrowserRouter>
   );
