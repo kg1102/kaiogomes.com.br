a0f8576f9bc4d521f8577d8a0898793c63963bb3
Kaio Gomes <KaioGomes@bugcrowdninja.com>
2022-02-07 03:38:25 -0300

    Added card__author and box shadow for 3D effect on cards!

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router';
 
 
 const CardContainer = styled.div`
-    background-color: black;
     height: 18rem;
     border-radius: 15px;
     margin-top: 25px;
     display: inline-block;
     flex-grow: 1;
     width: calc(100% * (1/4) - 10px - 1px);
+    box-shadow: -2px -2px 4px;
+    background-color: black; 
+
 
     @media screen and (max-width: 939px){
         width: calc(100% * (1/4) - 10px - 1px);
@@ -32,6 +34,8 @@ const CardContainer = styled.div`
         height: 100%;
         box-shadow: inset 0px 0px 400px 510px rgba(0, 0, 0, .7);
         .card__content {
+            /* display: flex; */
+            /* justify-content: space-between; */
             .card__title {
                 overflow: hidden;
                 white-space: pre-wrap;      /* CSS3 */   
@@ -43,12 +47,20 @@ const CardContainer = styled.div`
                 color: white;
                 font-size: 1.1rem;
             }
+            .card__author {
+                padding: 0.3rem 2rem;
+                color: #FFF;
+                margin: 0 5%;
+                background-color: #4e2470;
+                border-radius: 10px;
+            }
         }
     }
 
     .card:hover {
         transition: 0.5s ease all;
-        transform: scale(1.03);
+        -webkit-transform: translateY(-5px);
+        transform: translateY(-5px);
     }
 
 `;
@@ -67,6 +79,7 @@ export function Card(props: CardProps) {
             <div className="card" style={{backgroundImage: `url("${props.image}")`}} onClick={() => navigate(`/blog/${props.path_id}`)}> 
                 <div className="card__content">
                     <h1 className="card__title">{props.title}</h1>
+                    <span className="card__author">Author: Kaio Gomes</span>
                 </div>
             </div>
         </CardContainer>
