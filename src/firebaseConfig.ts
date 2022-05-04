41a24bfe74ffa1c9def82005f15ef3daa7acf1bd
Linus Torvalds <torvalds@linux-foundation.org>
2022-05-03 23:19:13 -0300

    removing secret keys leakage

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBTSo7sZaCd56zK8GZqY9l2SI0jAsT4CBw",
-    authDomain: "kaiogomes.firebaseapp.com",
-    databaseURL: "https://kaiogomes-default-rtdb.firebaseio.com",
-    projectId: "kaiogomes",
-    storageBucket: "kaiogomes.appspot.com",
-    messagingSenderId: "941964012963",
-    appId: "1:941964012963:web:cc4bad5347f498fc9b7e16"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
