e565abb4987b2bffb24aff17f6cd52ee6aa47bcf
Linus Torvalds <torvalds@linux-foundation.org>
2022-05-04 02:15:25 -0300

    Banner updated

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -26,7 +26,7 @@ const Container = styled.div`
             border-radius: 15px;
             justify-content: center;
             align-items: center;
-            background-image:  url("https://api.creavite.co/out/cs6ZfZOeV3ra45xo_static.png");
+            background-image:  url("https://img.wattpad.com/ffbb2771e97a216672154fe539a9afacaa4b47f0/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5a6b7842753749356f65686d77413d3d2d3637323133363231352e313635313464363764313836306262633936313637353332393833382e676966");
             background-position: center;
             background-size: 100%;
             background-repeat: no-repeat;
